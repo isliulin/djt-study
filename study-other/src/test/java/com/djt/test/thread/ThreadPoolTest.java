cbca321c9432222b6b2e5125ad00ddc574c85bcd
段佳涛 <djt317@139.com>
2021-06-03 11:52:05 +0800

    other：增加ReentrantReadWriteLock测试

--- study-other/src/test/java/com/djt/test/thread/ThreadPoolTest.java
+++ study-other/src/test/java/com/djt/test/thread/ThreadPoolTest.java
@@ -1,10 +1,18 @@
 package com.djt.test.thread;
 
+import cn.hutool.core.thread.ThreadUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Random;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 线程池测试类
@@ -14,6 +22,16 @@ import java.util.concurrent.*;
  */
 @Slf4j
 public class ThreadPoolTest {
+
+    @Before
+    public void before() {
+        log.info("程序开始运行...");
+    }
+
+    @After
+    public void after() {
+        log.info("程序运行结束...");
+    }
 
     @Test
     public void testThreadPool1() {
@@ -43,12 +61,79 @@ public class ThreadPoolTest {
         while (!executor.isTerminated()) {
             log.info("getTaskCount={} getActiveCount={} getCompletedTaskCount={}",
                     executor.getTaskCount(), executor.getActiveCount(), executor.getCompletedTaskCount());
-            try {
-                Thread.sleep(1000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            ThreadUtil.sleep(1000);
         }
         log.info("所有线程执行完成...");
     }
+
+    @Test
+    public void testLock() {
+        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock(false);
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
+
+        AtomicBoolean done = new AtomicBoolean(false);
+        //启动线程1 获取读锁
+        executor.execute(() -> lockAndDo(readWriteLock, "READ", () -> {
+            while (!done.get()) {
+                ThreadUtil.sleep(10);
+            }
+        }));
+
+        ThreadUtil.sleep(1000);
+
+        //启动线程2 获取写锁
+        executor.execute(() -> lockAndDo(readWriteLock, "WRITE", () -> {
+            while (!done.get()) {
+                ThreadUtil.sleep(10);
+            }
+        }));
+
+        ThreadUtil.sleep(1000);
+
+        //启动线程3 获取读锁
+        executor.execute(() -> lockAndDo(readWriteLock, "READ", () -> {
+            while (!done.get()) {
+                ThreadUtil.sleep(10);
+            }
+        }));
+
+        executor.shutdown();
+        //10秒之后结束程序
+        ThreadUtil.sleep(10000);
+        done.set(true);
+        ThreadUtil.sleep(1000);
+
+    }
+
+    /**
+     * 加锁并执行任务
+     *
+     * @param readWriteLock 读写锁
+     * @param lockType      读/写
+     * @param task          待执行的任务
+     */
+    private void lockAndDo(ReadWriteLock readWriteLock, String lockType, Runnable task) {
+        Lock lock;
+        switch (StringUtils.trimToEmpty(lockType).toUpperCase()) {
+            case "READ":
+                lock = readWriteLock.readLock();
+                break;
+            case "WRITE":
+                lock = readWriteLock.writeLock();
+                break;
+            default:
+                throw new IllegalArgumentException(lockType + " 参数错误");
+        }
+        log.info("{} 尝试获取 {} 锁...", Thread.currentThread().getName(), lockType);
+        lock.lock();
+        try {
+            log.info("{} 成功获取 {} 锁...", Thread.currentThread().getName(), lockType);
+            task.run();
+        } finally {
+            lock.unlock();
+            log.info("{} 已经释放 {} 锁...", Thread.currentThread().getName(), lockType);
+        }
+    }
+
+
 }
