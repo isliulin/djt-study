fac25187c3d15650cdd0a8e139e69291371fd55e
段佳涛 <djt317@139.com>
2021-07-12 10:09:13 +0800

    other：配置整理

--- study-other/src/main/java/com/djt/kafka/KafkaDemo.java
+++ study-other/src/main/java/com/djt/kafka/KafkaDemo.java
@@ -33,25 +33,25 @@ public class KafkaDemo {
     public static Producer<String, String> createProducer(Properties props) {
         Properties kafkaProps = new Properties();
         kafkaProps.put(ProducerConfig.ACKS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_ACKS_CONFIG, "all"));
+                ConfigConstants.Kafka.KAFKA_ACKS_CONFIG, "all"));
         kafkaProps.put(ProducerConfig.RETRIES_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_RETRIES_CONFIG, "0"));
+                ConfigConstants.Kafka.KAFKA_RETRIES_CONFIG, "0"));
         kafkaProps.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_COMPRESSION_TYPE_CONFIG, "snappy"));
+                ConfigConstants.Kafka.KAFKA_COMPRESSION_TYPE_CONFIG, "snappy"));
         kafkaProps.put(ProducerConfig.BATCH_SIZE_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_BATCH_SIZE_CONFIG, "100"));
+                ConfigConstants.Kafka.KAFKA_BATCH_SIZE_CONFIG, "100"));
         kafkaProps.put(ProducerConfig.LINGER_MS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_LINGER_MS_CONFIG, "0"));
+                ConfigConstants.Kafka.KAFKA_LINGER_MS_CONFIG, "0"));
         kafkaProps.put(ProducerConfig.BUFFER_MEMORY_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_BUFFER_MEMORY_CONFIG, "33554432"));
+                ConfigConstants.Kafka.KAFKA_BUFFER_MEMORY_CONFIG, "33554432"));
         kafkaProps.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, props.getProperty(
-                ConfigConstants.KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1"));
+                ConfigConstants.Kafka.KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1"));
         kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_BOOTSTRAP_SERVERS_CONFIG));
+                ConfigConstants.Kafka.KAFKA_BOOTSTRAP_SERVERS_CONFIG));
         kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"));
+                ConfigConstants.Kafka.KAFKA_KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"));
         kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"));
+                ConfigConstants.Kafka.KAFKA_VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"));
         return new KafkaProducer<>(kafkaProps);
     }
 
@@ -64,27 +64,27 @@ public class KafkaDemo {
     public static Consumer<String, String> createConsumer(Properties props) {
         Properties kafkaProps = new Properties();
         kafkaProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_BOOTSTRAP_SERVERS_CONFIG));
+                ConfigConstants.Kafka.KAFKA_BOOTSTRAP_SERVERS_CONFIG));
         kafkaProps.put(ConsumerConfig.GROUP_ID_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_GROUP_ID_CONFIG));
+                ConfigConstants.Kafka.KAFKA_GROUP_ID_CONFIG));
         kafkaProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_ENABLE_AUTO_COMMIT_CONFIG, "true"));
+                ConfigConstants.Kafka.KAFKA_ENABLE_AUTO_COMMIT_CONFIG, "true"));
         kafkaProps.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_MAX_POLL_INTERVAL_MS_CONFIG, "1000"));
+                ConfigConstants.Kafka.KAFKA_MAX_POLL_INTERVAL_MS_CONFIG, "1000"));
         kafkaProps.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_MAX_POLL_RECORDS_CONFIG, "2"));
+                ConfigConstants.Kafka.KAFKA_MAX_POLL_RECORDS_CONFIG, "2"));
         kafkaProps.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000"));
+                ConfigConstants.Kafka.KAFKA_AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000"));
         kafkaProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_AUTO_OFFSET_RESET_CONFIG, "earliest"));
+                ConfigConstants.Kafka.KAFKA_AUTO_OFFSET_RESET_CONFIG, "earliest"));
         kafkaProps.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_SESSION_TIMEOUT_MS_CONFIG, "30000"));
+                ConfigConstants.Kafka.KAFKA_SESSION_TIMEOUT_MS_CONFIG, "30000"));
         kafkaProps.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_HEARTBEAT_INTERVAL_MS_CONFIG, "1000"));
+                ConfigConstants.Kafka.KAFKA_HEARTBEAT_INTERVAL_MS_CONFIG, "1000"));
         kafkaProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer"));
+                ConfigConstants.Kafka.KAFKA_KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer"));
         kafkaProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, props.getProperty(
-                ConfigConstants.KAFKA_VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer"));
+                ConfigConstants.Kafka.KAFKA_VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer"));
         return new KafkaConsumer<>(kafkaProps);
     }
 
